7d388b918c9eb966014a8bb6d300f80d5945e1ba
Benita <benitaacosta1995@gmail.com>
2019-02-05 16:56:30 -0600

    variables commit

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-alert("Welcome to my page!");
+var person = prompt("Welcome to my page!\nWhat is your name?", "Enter Name");
+if (person == null || person == "") {
+  txt = "You pressed Cancel!.";
+} else {
+    alert("Hello " + person + "! Click OK to continue.");
+}
+
+var name = "Benita";
+var age = 23;
+document.write("My name is " + name + " and I am " + age);
+
+var a = 2
+var b = 5
+var sum = a+b
+console.log("The sum is " + sum);
